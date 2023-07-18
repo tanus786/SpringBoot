2f0b3db13e8677b0f938104b65af1e35df247108
Tanu soni <Tanusoni2312@gmail.com>
2023-07-18 19:12:59 +0530

    Crud operation

--- BootJPA/src/main/java/com/jpa/test/BootJpaApplication.java
+++ BootJPA/src/main/java/com/jpa/test/BootJpaApplication.java
@@ -1,13 +1,78 @@
 package com.jpa.test;
 
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.jpa.test.dao.UserRepository;
+import com.jpa.test.entities.User;
 
 @SpringBootApplication
 public class BootJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootJpaApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(BootJpaApplication.class, args);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+//		User user = new User();
+//		user.setName("Tanu Soni");
+//		user.setStatus("Java Developer");
+//		user.setCity("Delhi");
+//		User user1 = userRepository.save(user);
+//		System.out.println(user1);
+
+//		// Create Object of User
+//		User user = new User();
+//		user.setName("Updesh");
+//		user.setStatus("Unity Developer");
+//		user.setCity("Aligarh");
+//
+//		// Saving Single User
+////		User user1 = userRepository.save(user);
+////		System.out.println(user1);
+//
+//		User user2 = new User();
+//		user2.setName("Vanshika");
+//		user2.setStatus("Python Developer");
+//		user2.setCity("Delhi");
+////		User user3 = userRepository.save(user2);
+////		System.out.println(user3);
+//
+//		// Saving More than a single User
+//		List<User> users = List.of(user, user2);
+//		Iterable<User> allUser = userRepository.saveAll(users);
+//		allUser.forEach(usr -> {
+//			System.out.println(usr);
+//		});
+
+		// Update User
+
+//		Optional<User> optional = userRepository.findById(52);
+//		User user = optional.get();
+//		user.setName("Jay Ojha");
+//		User finaluser = userRepository.save(user);
+//		System.out.println(finaluser);
+
+		// How to get data
+		
+		// findById(id) - return optional containing your data
+//		Optional<User> optional = userRepository.findById(52);
+		
+//		Iterable<User> allUser = userRepository.findAll();
+//		allUser.forEach(user ->{
+//			System.out.println(user);
+//		});
+		
+		//Delete the object
+//		
+//		userRepository.deleteById(52);
+//		System.out.println("deleted");
+		
+		
+		
+		
+		
 	}
 
 }
---
+++ BootJPA/src/main/java/com/jpa/test/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.jpa.test.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import com.jpa.test.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	
+}
