21cfd106826d02e4a36236820835d857f0a887f7
Tanu soni <Tanusoni2312@gmail.com>
2023-07-24 14:12:35 +0530

    Loop

--- ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
+++ ThymeleafPractical/src/main/java/com/practice/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.practice.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
-
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,7 +18,18 @@ public class MyController {
 		System.out.println("Going Inside About Handler");
 		model.addAttribute("name", "Tanu Soni");
 		model.addAttribute("date", new Date());
-		
 		return "about";
 	}
+	//Loop in Thymeleaf
+	@GetMapping("/loop")
+	public String iterateHandler(Model model) {
+		List<String> list = new ArrayList<>();
+		list.add("Tanu");
+		list.add("Updesh");
+		list.add("Vanshika");
+		list.add("Vaibhav");
+		model.addAttribute("friends",list);
+		return "iterate";
+	}
+	
 }
